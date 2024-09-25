42fa96f0e56528ae431537c13871f8921ef4059a
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-25 14:11:54 +0200

    added: AppHeader component, Yu-Gi-Oh logo, search input

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+
+
+
+<template>
+
+  <AppHeader />
+
+</template>
+
+
+
+
+
+<style lang="scss" scoped></style>
