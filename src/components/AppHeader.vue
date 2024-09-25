36df37f6f366bfc6685f4ccdcb1d9bf004663213
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-09-25 14:11:54 +0200

    added: AppHeader component, Yu-Gi-Oh logo, search input

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+
+
+<template>
+    <header>
+
+        <div class="LeftSide">
+            <img src="https://1000logos.net/wp-content/uploads/2024/02/Yu-Gi-Oh-Logo-2001.png" alt="Yughi Logo"
+                class="logo">
+            <h1>Yu-Gi-Oh Card List</h1>
+        </div>
+
+
+
+        <div>
+            <input type="text" placeholder="Test" class="SearchBar">
+        </div>
+
+    </header>
+</template>
